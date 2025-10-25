1e58c2329a9fd2ddf24b39ab5ac50fc2da3b6456
madman1095 <so2softplut@gmail.com>
2025-10-25 04:02:36 +0300

    Update nh.lua

--- nh.lua
+++ nh.lua
@@ -1,6 +1,6 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
 
-local Window = Library.CreateLib("Name", "NOHCHO HUB")
+local Window = Library.CreateLib("NOHCHO HUB", "RJTheme3")
 
 local Tab = Window:NewTab("Players")
 
