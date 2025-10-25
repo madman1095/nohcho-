31460ce893830533a6760fa6f61a1795a8c6a157
madman1095 <so2softplut@gmail.com>
2025-10-25 03:59:21 +0300

    Replace OrionLib with new UI library and add toggle
    Updated the UI library and added a quick jump toggle feature.

--- nh.lua
+++ nh.lua
@@ -1,8 +1,18 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "NOHCHO HUB | Violence District", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/Tuturial_UI_Library/main/UI_Template_1"))()
+
+local Window = Library.CreateLib("Name", "NOHCHO HUB")
+
+local Tab = Window:NewTab("Players")
+
+local Section = Tab:NewSection("Quick Jump")
+
+Section:NewToggle("ToggleText", "ToggleInfo", function(state)
+    if state then
+        while wait() do
+          game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 200
+        end
+    else
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 20
+    end
+end)
   
-  
-  
-  
-end
-OrionLib:Init()
