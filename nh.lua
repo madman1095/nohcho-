d1733dca5836115cd827b62f7fc6cf35e5c81c27
madman1095 <so2softplut@gmail.com>
2025-10-25 13:39:46 +0300

    Update nh.lua

--- nh.lua
+++ nh.lua
@@ -48,12 +48,5 @@ Rayfield:Notify({
    Image = 4483362458,
 })
 
-local Button = MainTab:CreateButton({
-   Name = "ESP",
-   Callback = function()
-   _G.FriendColor = Color3.fromRGB(0, 0, 255)
-_G.EnemyColor = Color3.fromRGB(255, 0, 0)
-_G.UseTeamColor = true
-
  end,
 })
