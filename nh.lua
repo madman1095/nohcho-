4969aa86311a5d62f0374c6ba9bcd107196f2232
madman1095 <so2softplut@gmail.com>
2025-10-25 13:46:30 +0300

    Rename variables for clarity in nh.lua

--- nh.lua
+++ nh.lua
@@ -37,9 +37,9 @@ local Window = Rayfield:CreateWindow({
       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
-local Tab = Window:CreateTab("Players", nil)
+local MainTab = Window:CreateTab("Players", nil)
 
-local Section = Tab:CreateSection("Quickly climb")
+local MainSection = MainTab:CreateSection("Quickly climb")
 
 Rayfield:Notify({
    Title = "Notification Title",
