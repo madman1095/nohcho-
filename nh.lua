0faa40ba01ca07223ffa706407803f38bc1aeb2d
madman1095 <so2softplut@gmail.com>
2025-10-25 03:37:52 +0300

    Update nh.lua

--- nh.lua
+++ nh.lua
@@ -1,4 +1,3 @@
-if game.PlaceId == 93978595733734 then
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/jensonhirst/Orion/main/source')))()
 local Window = OrionLib:MakeWindow({Name = "NOHCHO HUB | Violence District", HidePremium = false, SaveConfig = true, ConfigFolder = "OrionTest"})
   
